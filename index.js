1dcd4b24f3459c4877df4273ba2658e0adb8d4b3
20pa1a1253 <20pa1a1253@vishnu.edu.in>
2022-11-26 19:44:51 +0530

    secondcommit

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
+const fs = require('fs')
+const http = require('http')
+let homeContent = ''
+let projectContent = ''
+let registrationContent=''
 
-function hello () {
-  console.log('Hello Node.Js!')
-}
-hello()
+fs.readFile('home.html', (err, home) => {
+  if (err) {
+    throw err
+  }
+  homeContent = home
+});
+fs.readFile('project.html', (err, project) => {
+  if (err) {
+    throw err
+  }
+  projectContent = project
+})
+fs.readFile('registration.html', (err, register) => {
+  if (err) {
+    throw err
+  }
+  registrationContent = register
+})
+const port = require('minimist')(process.argv.slice(2))
+http.createServer((request, response) => {
+  let url = request.url
+  response.writeHeader(200, { 'Content-Type': 'text/html' })
+  switch (url) {
+    case '/project':
+      response.write(projectContent)
+      response.end()
+      break
+    case '/registration':
+      response.write(registrationContent)
+      response.end()
+      break
+    default:
+      response.write(homeContent)
+      response.end()
+      break
+  }
+}).listen(port)
