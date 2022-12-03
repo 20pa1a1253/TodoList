e98230f5274114c75020fc1791a43d682082f3f5
20pa1a1253 <20pa1a1253@vishnu.edu.in>
2022-12-03 18:30:27 +0530

    level4commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const todoList = () => {
   all = []
+
   const add = (todoItem) => {
     all.push(todoItem)
   }
@@ -8,19 +9,22 @@ const todoList = () => {
   }
 
   const overdue = () => {
-    const a=formattedDate(new Date(new Date().setDate(dateToday.getDate() - 1)));
-    let b=[];
-      for(let i=0;i<all.length;i++){
-          if(all[i].dueDate==a){
-              b.push(all[i]);
-
-          }
+    var z = new Date();
+    z.setDate(z.getDate() - 1);
+    const a = z.toLocaleDateString("en-CA");
+    // const a=formattedDate(new Date(new Date().setDate(dateToday.getDate() - 1)));
+    let b = [];
+    for (let i = 0; i < all.length; i++) {
+      if (all[i].dueDate == a) {
+        //console.log(all[i]);
+        b.push(all[i]);
       }
-  return b;
-  }
+    }
+    return b;
+  };
   const dueToday = () => {
-    const a=formattedDate(dateToday);
-    let b=[];
+     b=[];
+    const a=new Date().toLocaleDateString("en-CA");
       for(let i=0;i<all.length;i++){
           if(all[i].dueDate==a){
               b.push(all[i]);
@@ -30,16 +34,19 @@ const todoList = () => {
   return b;
   }
   const dueLater = () => {
-    const a= formattedDate(new Date(new Date().setDate(dateToday.getDate() + 1)))
-    let b=[];
-    for(let i=0;i<all.length;i++){
-        if(all[i].dueDate==a){
-            b.push(all[i]);
-
-        }
+    var z = new Date();
+    z.setDate(z.getDate() + 1);
+    var a = z.toLocaleDateString("en-CA");
+    let b = [];
+    for (let i = 0; i < all.length; i++) {
+      if (all[i].dueDate == a) {
+        //console.log(all[i]);
+        b.push(all[i]);
+      }
     }
-  return b;
-  }
+    return b;
+  };
+  
   const toDisplayableList = (list) => {
     let s=""
     for(let i=0;i<list.length;i++){
@@ -51,7 +58,7 @@ const todoList = () => {
           s=s+"[ "+"] ";
       }
       const a=formattedDate(dateToday);
-      const b=formattedDate(new Date(new Date().setDate(dateToday.getDate() + 1)))
+      const b=formattedDate(new Date(new Date().setDate(dateToday.getDate() + 1)));
       if(list[i].dueDate==a){
           s=s+list[i].title+" "+"\n";
       }
@@ -70,47 +77,9 @@ const todoList = () => {
 
   return { all, add, markAsComplete, overdue, dueToday, dueLater, toDisplayableList };
 }
+module.exports=todoList;
 
 // ####################################### #
 // DO NOT CHANGE ANYTHING BELOW THIS LINE. #
 // ####################################### #
 
-const todos = todoList();
-
-const formattedDate = d => {
-  return d.toISOString().split("T")[0]
-}
-
-var dateToday = new Date()
-const today = formattedDate(dateToday)
-const yesterday = formattedDate(
-  new Date(new Date().setDate(dateToday.getDate() - 1))
-)
-const tomorrow = formattedDate(
-  new Date(new Date().setDate(dateToday.getDate() + 1))
-)
-todos.add({ title: 'Submit assignment', dueDate: yesterday, completed: false })
-todos.add({ title: 'Pay rent', dueDate: today, completed: true })
-todos.add({ title: 'Service Vehicle', dueDate: today, completed: false })
-todos.add({ title: 'File taxes', dueDate: tomorrow, completed: false })
-todos.add({ title: 'Pay electric bill', dueDate: tomorrow, completed: false })
-
-console.log("My Todo-list\n\n")
-
-console.log("Overdue")
-var overdues = todos.overdue()
-var formattedOverdues = todos.toDisplayableList(overdues)
-console.log(formattedOverdues)
-console.log("\n\n")
-
-console.log("Due Today")
-let itemsDueToday = todos.dueToday()
-let formattedItemsDueToday = todos.toDisplayableList(itemsDueToday)
-console.log(formattedItemsDueToday)
-console.log("\n\n")
-
-console.log("Due Later")
-let itemsDueLater = todos.dueLater()
-let formattedItemsDueLater = todos.toDisplayableList(itemsDueLater)
-console.log(formattedItemsDueLater)
-console.log("\n\n")
